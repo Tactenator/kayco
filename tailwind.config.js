2f5929ec9685b483a26ac98b94d161663482f25d
Tactenator <trevor.mclaurine@gmail.com>
2023-11-16 15:19:32 -0600

    working on products area

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     './**.{html,js}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'background': '#fafafa'
+      },
+      colors: {
+        'text': '#260d0d',
+        'background': '#fafafa',
+        'primary': '#205b5b',
+        'secondary': '#eee6e2',
+        'accent': '#42bd80',
+       }
+    },
   },
   plugins: [],
 }
