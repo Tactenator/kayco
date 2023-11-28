23f74319042c738471498aa191b8091f8b734e65
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-11-28 10:42:06 -0600

    working on checkout

--- src/script.js
+++ src/script.js
@@ -215,4 +215,31 @@ document.addEventListener("DOMContentLoaded", function() {
             handlePrice(cart)
         })
     }
+
+    const checkoutButton = document.getElementById('checkout-button')
+    checkoutButton.addEventListener('click', () => {
+        handleCheckout()
+    })
+
+    function handleCheckout() {
+        fetch("create-checkout-session", {
+            method: "POST", 
+            headers: {
+                "Content-type": "application/json", 
+            }, 
+            body: json.stringify({
+                items: [
+                    { id: 1, quantity: 3 }, 
+                    { id: 2, quantity: 1 }
+                ]
+            })
+        })
+            .then(res => {
+                if (res.ok) return res.json()
+                return res.json().then(json => Promise.reject(json))
+            })
+            .then(({ url}) => {
+                console.log(url)
+            })
+    }
 });
