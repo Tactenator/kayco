6f6ec00d64f0f3e87f3cfc28672f7adbf7a20618
Tactenator <trevor.mclaurine@gmail.com>
2023-12-07 10:23:54 -0600

    animation working

--- src/animations.js
+++ src/animations.js
@@ -7,14 +7,19 @@ document.addEventListener("DOMContentLoaded", function() {
       });
       gsap.from("#hero-text p", { 
         duration: 0.75,
+        delay: 0.5,
         opacity: 0, 
         y: '100%',
         ease: 'power4inOut'
       });
 
-    gsap.from("#hero-text a", {
-        opacity: 0,
-        duration: 1.00,  
-        ease: 'power4inOut' 
+      gsap.fromTo("#hero-text a", {
+        opacity: 0, 
+    }, 
+    {
+        opacity: 1, 
+        delay: 1,
+        duration: 1, 
+        ease: 'power4.inOut'
     })
 });
