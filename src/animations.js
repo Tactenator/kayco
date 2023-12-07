562481ce6a9f21d3d6a2ad80c93b992aaf459dc3
Tactenator <trevor.mclaurine@gmail.com>
2023-12-07 10:14:17 -0600

    working on landing animation

--- src/animations.js
+++ src/animations.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    gsap.from("#hero-text", { 
-        duration: 0.5,
+    gsap.from("#hero-text h2", { 
+        duration: 0.75,
         opacity: 0, 
-        y: '20%',
+        y: '100%',
         ease: 'power4inOut'
       });
+      gsap.from("#hero-text p", { 
+        duration: 0.75,
+        opacity: 0, 
+        y: '100%',
+        ease: 'power4inOut'
+      });
+
+    gsap.from("#hero-text a", {
+        opacity: 0,
+        duration: 1.00,  
+        ease: 'power4inOut' 
+    })
 });
